8117a947d67582990704955906ba0ea9d224da84
vaibs <vaibhavmuratkar2@gmail.com>
2021-12-12 15:37:11 +0530

    Length of Line-UC1

--- src/com/bridgelabz/basiccoreprogram/lineComparision.java
+++ src/com/bridgelabz/basiccoreprogram/lineComparision.java
@@ -2,6 +2,17 @@ package com.bridgelabz.basiccoreprogram;
 
 public class lineComparision {
     public static void main(String[] args) {
-        
+        // X and Y Co-ordinates
+        int X1 = 10;
+        int X2 = 20;
+        int Y1 = 11;
+        int Y2 = 24;
+
+        // Length of Line
+        double squareOfX = Math.pow((X2-X1),2);
+        double squareOfY = Math.pow((Y2-Y1),2);
+        double Length_of_Line = Math.sqrt(squareOfX + squareOfY);
+        System.out.println(" Length of Line: " + Length_of_Line);
+
     }
 }
